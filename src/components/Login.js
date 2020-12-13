8936e244799ead7c8ddb4ac2311c551999b9cf16
Ganesh <tyganeshbabu@gmail.com>
2020-12-13 23:19:19 +0530

    Added login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,35 +1,59 @@
-import React from 'react';
-import { useGoogleLogin } from 'react-google-login';
-import { refreshTokenSetup }  from '../utils/refreshToken';
+import React from "react";
+import "./Login.css";
+import logo from '../assets/logo.png';
 
-const clientId =
-  '824296096613-hj384bdkbqb1gfa0dpjpq11dbsjljdr2.apps.googleusercontent.com';
-
-const Login = (props) => {
-  const onSuccess = (res) => {
-    console.log(res);
-    console.log('inside success');
-    localStorage.setItem('authToken', res.tokenObj.id_token);
-    refreshTokenSetup(res);
-    props.toggleBtn('login');
-  };
-
-  const onFailure = (res) => {
-    console.log('Login failed: res:', res);
-  };
-
-  const { signIn } = useGoogleLogin({
-    onSuccess,
-    onFailure,
-    clientId,
-    isSignedIn: true,
-    accessType: 'offline'
-  });
-
+const Login = () => {
+  const onSubmit = (event) => {
+    console.log('submitted');
+    event.preventDefault();
+  }
   return (
-    <button onClick={signIn} className="button">
-      <span className="buttonText">Sign in with Google</span>
-    </button>
+    <div className="login-main">
+      <div className="login-container">
+        <div className="login-center">
+          <div className="login-middle">
+            <div id="loginform">
+              {/* <div className="linkStyle ml-4 mb-2">
+                <h4>Login</h4>
+              </div> */}
+              <form onClick={onSubmit}>
+                <fieldset className="login-fieldset">
+                  <p>
+                    <span className="fa fa-user login-user-icon"></span
+                    ><input
+                      className="login-user"
+                      type="text"
+                      placeholder="Username"
+                    />
+                  </p>
+                  <p>
+                    <span className="fa fa-lock login-pass-icon"></span
+                    ><input
+                      className="login-pass"
+                      type="password"
+                      placeholder="Password"
+                    />
+                  </p>
+                  <div>
+                    <input
+                      type="submit"
+                      value="Sign In"
+                      className="btn btn-md submit-btn"
+                    />
+                  </div>
+                </fieldset>
+              </form>
+              <div className="login-logo-mob">
+                <img alt="Foodie Spot Logo" src={logo} className="login-logo-img-mob ml-5" />
+              </div>
+            </div>
+            <div className="login-logo">
+              <img alt="Foodie Spot Logo" src={logo} className="login-logo-img" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
